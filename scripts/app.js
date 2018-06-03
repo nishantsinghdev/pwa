c8b5e8cca00f3321d2dd66bc0bafc06221a11010
Nishant Singh <nishjan1996@gmail.com>
2018-06-03 13:00:38 +0530

    Update in Service Worker and Manifest

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,27 @@
+/*
 if ('serviceWorker' in navigator) {
+
   window.addEventListener('load', function() {
+
     navigator.serviceWorker.register('/scripts/sw.js').then(function(registration) {
+
       // Registration was successful
       console.log('Service Worker Registered Successfully with scope : ', registration.scope);
+
     }, function(err) {
+
       // registration failed :(
       console.log('Service Worker Registration Failed with error : ', err);
+      
     });
-  });
-}
 
+  });
+
+}
+*/
+
+"serviceWorker" in navigator && (console.log("Will service worker register?"), navigator.serviceWorker.register("scripts/sw.js").then(function(e) {
+  console.log('Service Worker Registered Successfully with scope : ', registration.scope)
+}).catch(function(e) {
+  console.log('Service Worker Registration Failed with error : ', e)
+}));
--- scripts/sw.js
+++ scripts/sw.js
@@ -1,12 +1,8 @@
-var dataCacheName = 'myApp';
-var cacheName = 'myAppCache';
+var dataCacheName = 'PWA';
+var cacheName = 'PWA-Cache';
 var filesToCache = [
   '/',
-  '/index.html',
-  '/scripts/app.js',
-  '/scripts/sw.js',
-  '/styles/style.css',
-  '/assets/images/pwa.png'
+  'index.html'
 ];
 
 self.addEventListener('install', function(e) {
@@ -19,26 +15,63 @@ self.addEventListener('install', function(e) {
   );
 });
 
-self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
-  e.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (key !== cacheName && key !== dataCacheName) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
+
+  self.addEventListener('activate', function(event) {
+
+  var cacheWhitelist = ['static-cache'];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
-  return self.clients.claim();
 });
-
+  /*
+   * Fixes a corner case in which the app wasn't returning the latest data.
+   * You can reproduce the corner case by commenting out the line below and
+   * then doing the following steps: 1) load app for first time so that the
+   * initial New York City data is shown 2) press the refresh button on the
+   * app 3) go offline 4) reload the app. You expect to see the newer NYC
+   * data, but you actually see the initial data. This happens because the
+   * service worker is not yet activated. The code below essentially lets
+   * you activate the service worker faster.
+   */
+  
 self.addEventListener('fetch', function(e) {
   console.log('[Service Worker] Fetch', e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+  var dataUrl = 'https://nishantsinghdev.github.io/pwa/';
+  if (e.request.url.indexOf(dataUrl) > -1) {
+    /*
+     * When the request URL contains dataUrl, the app is asking for fresh
+     * weather data. In this case, the service worker always goes to the
+     * network and then caches the response. This is called the "Cache then
+     * network" strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
+     */
+    e.respondWith(
+      caches.open(dataCacheName).then(function(cache) {
+        return fetch(e.request).then(function(response){
+          cache.put(e.request.url, response.clone());
+          return response;
+        });
+      })
+    );
+  } else {
+    /*
+     * The app is asking for app shell files. In this scenario the app uses the
+     * "Cache, falling back to the network" offline strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+     */
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  }
 });
